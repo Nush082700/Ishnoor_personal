75aa36e4101c7d7f30dec68f7d91454444e9df02
Anusha Choudhury <ac978@cornell.edu>
2020-07-19 14:14:49 -0400

    newspaper committed

--- app.py
+++ app.py
@@ -1,24 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request,redirect, url_for
 import os
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def main_page():
-    return render_template('home_page.html')
-    return "Hello World!"
-
-
-@app.route('/newsletter')
-def newsletter_msg():
-    return "this is the newsletter feature"
 
 
 @app.route('/pictures')
 def wall_pictures():
     return render_template('picture_wall.html')
     #return 'this is a wall of pictures'
+
+@app.route('/', methods = ['GET','POST'])
+def main_page():
+    if request.method == 'POST':
+        return redirect(url_for('wall_pictures'))
+    return render_template('home_page.html')
+    
+    # return "Hello World!"
+
+
+@app.route('/newsletter')
+def newsletter_msg():
+    return render_template('newspaper.html')
+    #return "this is the newsletter feature"
+
+
+
 
 
 @app.route('/wall')
