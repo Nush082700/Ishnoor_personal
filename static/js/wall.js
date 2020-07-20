6bbd775e1c8c4be6d11cba6ad9edde43d081280c
Anusha Choudhury <ac978@cornell.edu>
2020-07-20 03:55:25 -0400

    FINAL

--- static/js/wall.js
+++ static/js/wall.js
@@ -1,11 +1,24 @@
-var button = document.getElementById("brickwall"),
-  count = 0;
-button.onclick = function () {
-  count += 1;
-  if (count >= 20) {
-    document.getElementById("brickwall").style.display = "none";
-    document.getElementById("changetext").innerHTML =
-      "You broke the 4th wall! Now you gotta go to the repo and fix it!";
-    document.getElementById("github").style.display = "block";
+window.onload = function () {
+  var count = 0;
+  var i = 0;
+  var txt =
+    "You broke the fourth wall! Now you have to go to the repo to fix it!";
+  var speed = 50;
+  document.getElementById("brickwall").onclick = function () {
+    count += 1;
+    if (count >= 20) {
+      document.getElementById("brickwall").style.visibility = "hidden";
+      document.getElementById("changetext").innerHTML = "";
+      document.getElementById("github").style.visibility = "visible";
+      typeWriter();
+    }
+  };
+
+  function typeWriter() {
+    if (i < txt.length) {
+      document.getElementById("changetext").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
   }
 };
